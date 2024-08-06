519712bb69c4d70189905cadfcdceb9596fc9443
mangmuse <mangmangmuse@gmail.com>
2024-08-06 09:07:58 +0900

    confir: setup project

--- src/provider/Providers.tsx
+++ src/provider/Providers.tsx
@@ -1,5 +1,8 @@
-function Providers() {
-  return <></>;
+import QueryProvider from "@/provider/QueryProvider";
+import { ReactNode } from "react";
+
+function Providers({ children }: { children: ReactNode }) {
+  return <QueryProvider>{children}</QueryProvider>;
 }
 
 export default Providers;
