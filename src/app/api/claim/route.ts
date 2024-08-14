8bc6223f311fa214ed20b461539918ac204c10af
mangmuse <mangmangmuse@gmail.com>
2024-08-14 21:08:22 +0900

    fix: gift claim bug

--- src/app/api/claim/route.ts
+++ src/app/api/claim/route.ts
@@ -45,14 +45,12 @@ export const GET = async (req: NextRequest) => {
 export const PATCH = async (req: NextRequest) => {
   const supabase = createClient();
   const updatedClaim = await req.json();
-
   try {
     const { status, statusText } = await supabase
       .from("gift_claims")
       .update(updatedClaim)
-      .eq("gift_claimId", updatedClaim.gift_claimId)
+      .eq(`gift_claimId`, updatedClaim.gift_claimId)
       .single();
-
     return NextResponse.json({ status, statusText });
   } catch (e) {
     if (e instanceof Error) {
